cf0594672c85045ddf43e6a2ba7a884a0d571e2d
Aaron Asch <aaron.asch@gmail.com>
2020-04-14 07:19:57 -0400

    print the command in process_images.sh

--- process_images.sh
+++ process_images.sh
@@ -3,5 +3,7 @@ set -e;
 docker build -t pointillism .;
 for img_path in $(find ./images -type f); do
     res_path=$(echo $img_path | sed 's:images/:drawings/:');
-    docker run -v $(pwd):/usr/src/app -it --rm pointillism python main.py $img_path $res_path;
+    command="docker run -v $(pwd):/usr/src/app -it --rm pointillism python main.py $img_path $res_path";
+    echo $command;
+    $command;
 done;
